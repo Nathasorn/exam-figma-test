414916f5f741f058bc19c127e40f27d5d8d0bbb1
Nathasorn <69458276+Nathasorn@users.noreply.github.com>
2023-06-28 09:57:05 +0700

    update

--- src/components/Onepage.jsx
+++ src/components/Onepage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../scss/Onepage.scss'
+//Desktop/Header
 import Imgphone from "../assets/DesktopHeader/desktop-header-phone.png"
 import Facebook from "../assets/DesktopHeader/Facebook.png"
 import Line from "../assets/DesktopHeader/Line.png"
 import Language from "../assets/DesktopHeader/Language.png"
+//Navbar-Header
 import Logo from "../assets/NavbarHeader/Logo.png"
+//Home
+import Bghome from "../assets/Home/Home-Background.png"
+import Image225 from "../assets/Home/image225.png"
+import Image227 from "../assets/Home/image227.png"
 
 function onepage() {
     return (
@@ -25,7 +31,7 @@ function onepage() {
                     </div>
                 </div>
             </div>
-            <div className="Home">
+            <div className="Navbar">
                 <div className="Navbar-Header">
                     <div className="Logo">
                         <img className="logo" src={Logo} alt="Logo"></img>
@@ -37,7 +43,28 @@ function onepage() {
                         <a className="navbar-aboutus" href="aboutus">เกี่ยวกับเรา</a>
                         <a className="navbar-contactus" href="contactus">ติดต่อเรา</a>
                     </div>
-                    
+                    <div className="button-login">
+                        <div className="button-login-base">
+                            <a className="button-login-text">เข้าสู่ระบบ</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="Home">
+                <div className="reactangle26"></div>
+                <div className="reactangle27"></div>
+                <a className="reactangle27-text">NEW</a>
+                <a className="reactangle26-text">ตรวจสอบประวัติผ่านระบบ K-ETC</a>
+                <div className="home-group-header-text">
+                    <a className="home-group-header-text-detail">บริการยื่น ตรวจสอบประวัติอาชญากรรม สำหรับ องค์กร และบุคคลทั่วไป</a>
+                </div>
+                <div className="group1000003687">
+                <a className="image225-text">ไม่ต้องรอคิว</a>
+                </div>
+                <div className="home-image">
+                    <img className="home-background" src={Bghome} alt="Home-Background"></img>
+                    <img className="image225" src={Image225} alt="Image225"></img>
+                    <img className="image227" src={Image227} alt="Image227"></img>
                 </div>
             </div>
         </div>
